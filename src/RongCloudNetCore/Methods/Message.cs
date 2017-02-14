721db8e6768c4812f0c588c036bdfdaee48a243b
姚正发 <976691141@qq.com>
2017-02-14 18:02:59 +0800

    提交

---
+++ src/RongCloudNetCore/Methods/Message.cs
@@ -0,0 +1,282 @@
+﻿using Newtonsoft.Json;
+using RongCloudNetCore.Messages;
+using RongCloudNetCore.Models;
+using RongCloudNetCore.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace RongCloudNetCore.Methods
+{
+    public class Message
+    {
+        private string appKey;
+        private string appSecret;
+
+        public Message(string appKey, string appSecret)
+        {
+            this.appKey = appKey;
+            this.appSecret = appSecret;
+        }
+
+        /// <summary>
+        /// 发送单聊消息方法（一个用户向另外一个用户发送消息，单条消息最大 128k。每分钟最多发送 6000 条信息，每次发送用户上限为 1000 人，如：一次发送 1000 人时，示为 1000 条消息。） 
+        /// </summary>
+        /// <param name="fromUserId">发送人用户 Id。（必传）</param>
+        /// <param name="toUserId">接收用户 Id，可以实现向多人发送消息，每次上限为 1000 人。（必传）</param>
+        /// <param name="message">消息</param>
+        /// <param name="pushContent">定义显示的 Push 内容，如果 objectName 为融云内置消息类型时，则发送后用户一定会收到 Push 信息。如果为自定义消息，则 pushContent 为自定义消息显示的 Push 内容，如果不传则用户不会收到 Push 通知。（可选）</param>
+        /// <param name="pushData">针对 iOS 平台为 Push 通知时附加到 payload 中，Android 客户端收到推送消息时对应字段名为 pushData。（可选）</param>
+        /// <param name="count">针对 iOS 平台，Push 时用来控制未读消息显示数，只有在 toUserId 为一个用户 Id 的时候有效。（可选）</param>
+        /// <param name="verifyBlacklist">是否过滤发送人黑名单列表，0 表示为不过滤、 1 表示为过滤，默认为 0 不过滤。（可选）</param>
+        /// <param name="isPersisted">当前版本有新的自定义消息，而老版本没有该自定义消息时，老版本客户端收到消息后是否进行存储，0 表示为不存储、 1 表示为存储，默认为 1 存储消息。（可选）</param>
+        /// <param name="isCounted">当前版本有新的自定义消息，而老版本没有该自定义消息时，老版本客户端收到消息后是否进行未读消息计数，0 表示为不计数、 1 表示为计数，默认为 1 计数，未读消息数增加 1。（可选）</param>
+        public async Task<CodeSuccessReslut> PublishPrivate(string fromUserId, string[] toUserId, VoiceMessage message, string pushContent, string pushData, string count, int verifyBlacklist, int isPersisted, int isCounted)
+        {
+            if (string.IsNullOrEmpty(fromUserId))
+                throw new ArgumentNullException(nameof(fromUserId));
+            if (toUserId == null)
+                throw new ArgumentNullException(nameof(toUserId));
+            if (message.TYPE == null)
+                throw new ArgumentNullException(nameof(message.TYPE));
+
+            string postStr = "";
+            postStr += "fromUserId=" + WebUtility.UrlEncode(fromUserId == null ? "" : fromUserId) + "&";
+            for (int i = 0; i < toUserId.Length; i++)
+            {
+                String child = toUserId[i];
+                postStr += "toUserId=" + WebUtility.UrlEncode(child) + "&";
+            }
+
+            postStr += "objectName=" + WebUtility.UrlEncode(message.TYPE) + "&";
+            postStr += "content=" + WebUtility.UrlEncode(message.ToString()) + "&";
+            postStr += "pushContent=" + WebUtility.UrlEncode(pushContent == null ? "" : pushContent) + "&";
+            postStr += "pushData=" + WebUtility.UrlEncode(pushData == null ? "" : pushData) + "&";
+            postStr += "count=" + WebUtility.UrlEncode(count == null ? "" : count) + "&";
+            postStr += "verifyBlacklist=" + WebUtility.UrlEncode(Convert.ToString(verifyBlacklist) == null ? "" : Convert.ToString(verifyBlacklist)) + "&";
+            postStr += "isPersisted=" + WebUtility.UrlEncode(Convert.ToString(isPersisted) == null ? "" : Convert.ToString(isPersisted)) + "&";
+            postStr += "isCounted=" + WebUtility.UrlEncode(Convert.ToString(isCounted) == null ? "" : Convert.ToString(isCounted)) + "&";
+            postStr = postStr.Substring(0, postStr.LastIndexOf('&'));
+
+            return JsonConvert.DeserializeObject<CodeSuccessReslut>(await RongHttpClient.ExecutePost(appKey, appSecret, RongCloud.RONGCLOUDURI + "/message/private/publish.json", postStr, "application/x-www-form-urlencoded"));
+        }
+
+        /// <summary>
+        /// 发送单聊模板消息方法（一个用户向多个用户发送不同消息内容，单条消息最大 128k。每分钟最多发送 6000 条信息，每次发送用户上限为 1000 人。） 
+        /// </summary>
+        /// <param name="templateMessage">单聊模版消息</param>
+        public async Task<CodeSuccessReslut> PublishTemplate(TemplateMessage templateMessage)
+        {
+            if (templateMessage == null)
+                throw new ArgumentNullException(nameof(templateMessage));
+
+            string postStr = "";
+            postStr = JsonConvert.SerializeObject(templateMessage);
+            return JsonConvert.DeserializeObject<CodeSuccessReslut>(await RongHttpClient.ExecutePost(appKey, appSecret, RongCloud.RONGCLOUDURI + "/message/private/publish_template.json", postStr, "application/json"));
+        }
+
+        /// <summary>
+        /// 发送系统消息方法（一个用户向一个或多个用户发送系统消息，单条消息最大 128k，会话类型为 SYSTEM。每秒钟最多发送 100 条消息，每次最多同时向 100 人发送，如：一次发送 100 人时，示为 100 条消息。） 
+        /// </summary>
+        /// <param name="fromUserId">发送人用户 Id。（必传）</param>
+        /// <param name="toUserId">接收用户 Id，提供多个本参数可以实现向多人发送消息，上限为 1000 人。（必传）</param>
+        /// <param name="message">发送消息内容（必传）</param>
+        /// <param name="pushContent">如果为自定义消息，定义显示的 Push 内容，内容中定义标识通过 values 中设置的标识位内容进行替换.如消息类型为自定义不需要 Push 通知，则对应数组传空值即可。（可选）</param>
+        /// <param name="pushData">针对 iOS 平台为 Push 通知时附加到 payload 中，Android 客户端收到推送消息时对应字段名为 pushData。如不需要 Push 功能对应数组传空值即可。（可选）</param>
+        /// <param name="isPersisted">当前版本有新的自定义消息，而老版本没有该自定义消息时，老版本客户端收到消息后是否进行存储，0 表示为不存储、 1 表示为存储，默认为 1 存储消息。（可选）</param>
+        /// <param name="isCounted">当前版本有新的自定义消息，而老版本没有该自定义消息时，老版本客户端收到消息后是否进行未读消息计数，0 表示为不计数、 1 表示为计数，默认为 1 计数，未读消息数增加 1。（可选）</param>
+        public async Task<CodeSuccessReslut> PublishSystem(string fromUserId, string[] toUserId, TxtMessage message, String pushContent, String pushData, int isPersisted, int isCounted)
+        {
+            if (string.IsNullOrEmpty(fromUserId))
+                throw new ArgumentNullException(nameof(fromUserId));
+            if (toUserId == null)
+                throw new ArgumentNullException(nameof(toUserId));
+
+            string postStr = "";
+            postStr += "fromUserId=" + WebUtility.UrlEncode(fromUserId == null ? "" : fromUserId) + "&";
+            for (int i = 0; i < toUserId.Length; i++)
+            {
+                string child = toUserId[i];
+                postStr += "toUserId=" + WebUtility.UrlEncode(child) + "&";
+            }
+
+            postStr += "objectName=" + WebUtility.UrlEncode(message.TYPE) + "&";
+            postStr += "content=" + WebUtility.UrlEncode(message.ToString()) + "&";
+            postStr += "pushContent=" + WebUtility.UrlEncode(pushContent == null ? "" : pushContent) + "&";
+            postStr += "pushData=" + WebUtility.UrlEncode(pushData == null ? "" : pushData) + "&";
+            postStr += "isPersisted=" + WebUtility.UrlEncode(Convert.ToString(isPersisted) == null ? "" : Convert.ToString(isPersisted)) + "&";
+            postStr += "isCounted=" + WebUtility.UrlEncode(Convert.ToString(isCounted) == null ? "" : Convert.ToString(isCounted)) + "&";
+            postStr = postStr.Substring(0, postStr.LastIndexOf('&'));
+
+            return JsonConvert.DeserializeObject<CodeSuccessReslut>(await RongHttpClient.ExecutePost(appKey, appSecret, RongCloud.RONGCLOUDURI + "/message/system/publish.json", postStr, "application/x-www-form-urlencoded"));
+        }
+
+        /// <summary>
+        /// 发送系统模板消息方法（一个用户向一个或多个用户发送系统消息，单条消息最大 128k，会话类型为 SYSTEM.每秒钟最多发送 100 条消息，每次最多同时向 100 人发送，如：一次发送 100 人时，示为 100 条消息。） 
+        /// </summary>
+        /// <param name="templateMessage">系统模版消息</param>
+        public async Task<CodeSuccessReslut> PublishSystemTemplate(TemplateMessage templateMessage)
+        {
+            if (templateMessage == null)
+                throw new ArgumentNullException(nameof(templateMessage));
+
+            string postStr = "";
+            postStr = JsonConvert.SerializeObject(templateMessage);
+            return JsonConvert.DeserializeObject<CodeSuccessReslut>(await RongHttpClient.ExecutePost(appKey, appSecret, RongCloud.RONGCLOUDURI + "/message/system/publish_template.json", postStr, "application/json"));
+        }
+
+        /// <summary>
+        /// 发送群组消息方法（以一个用户身份向群组发送消息，单条消息最大 128k.每秒钟最多发送 20 条消息，每次最多向 3 个群组发送，如：一次向 3 个群组发送消息，示为 3 条消息。） 
+        /// </summary>
+        /// <param name="fromUserId">发送人用户 Id 。（必传）</param>
+        /// <param name="toGroupId">接收群Id，提供多个本参数可以实现向多群发送消息，最多不超过 3 个群组。（必传）</param>
+        /// <param name="message">发送消息内容（必传）</param>
+        /// <param name="pushContent">定义显示的 Push 内容，如果 objectName 为融云内置消息类型时，则发送后用户一定会收到 Push 信息. 如果为自定义消息，则 pushContent 为自定义消息显示的 Push 内容，如果不传则用户不会收到 Push 通知。（可选）</param>
+        /// <param name="pushData">针对 iOS 平台为 Push 通知时附加到 payload 中，Android 客户端收到推送消息时对应字段名为 pushData。（可选）</param>
+        /// <param name="isPersisted">当前版本有新的自定义消息，而老版本没有该自定义消息时，老版本客户端收到消息后是否进行存储，0 表示为不存储、 1 表示为存储，默认为 1 存储消息。（可选）</param>
+        /// <param name="isCounted">当前版本有新的自定义消息，而老版本没有该自定义消息时，老版本客户端收到消息后是否进行未读消息计数，0 表示为不计数、 1 表示为计数，默认为 1 计数，未读消息数增加 1。（可选）</param>
+        public async Task<CodeSuccessReslut> PublishGroup(string fromUserId, string[] toGroupId, TxtMessage message, string pushContent, string pushData, int isPersisted, int isCounted)
+        {
+            if (string.IsNullOrEmpty(fromUserId))
+                throw new ArgumentNullException(nameof(fromUserId));
+            if (toGroupId == null)
+                throw new ArgumentNullException(nameof(toGroupId));
+
+            string postStr = "";
+            postStr += "fromUserId=" + WebUtility.UrlEncode(fromUserId == null ? "" : fromUserId) + "&";
+            for (int i = 0; i < toGroupId.Length; i++)
+            {
+                string child = toGroupId[i];
+                postStr += "toGroupId=" + WebUtility.UrlEncode(child) + "&";
+            }
+
+            postStr += "objectName=" + WebUtility.UrlEncode(message.TYPE) + "&";
+            postStr += "content=" + WebUtility.UrlEncode(message.ToString()) + "&";
+            postStr += "pushContent=" + WebUtility.UrlEncode(pushContent == null ? "" : pushContent) + "&";
+            postStr += "pushData=" + WebUtility.UrlEncode(pushData == null ? "" : pushData) + "&";
+            postStr += "isPersisted=" + WebUtility.UrlEncode(Convert.ToString(isPersisted) == null ? "" : Convert.ToString(isPersisted)) + "&";
+            postStr += "isCounted=" + WebUtility.UrlEncode(Convert.ToString(isCounted) == null ? "" : Convert.ToString(isCounted)) + "&";
+            postStr = postStr.Substring(0, postStr.LastIndexOf('&'));
+
+            return JsonConvert.DeserializeObject<CodeSuccessReslut>(await RongHttpClient.ExecutePost(appKey, appSecret, RongCloud.RONGCLOUDURI + "/message/group/publish.json", postStr, "application/x-www-form-urlencoded"));
+        }
+
+        /// <summary>
+        /// 发送讨论组消息方法（以一个用户身份向讨论组发送消息，单条消息最大 128k，每秒钟最多发送 20 条消息.）
+        /// </summary>
+        /// <param name="fromUserId">发送人用户 Id（必传）</param>
+        /// <param name="toDiscussionId">接收讨论组 Id（必传）</param>
+        /// <param name="message">发送消息内容（必传）</param>
+        /// <param name="pushContent">定义显示的 Push 内容，如果 objectName 为融云内置消息类型时，则发送后用户一定会收到 Push 信息. 如果为自定义消息，则 pushContent 为自定义消息显示的 Push 内容，如果不传则用户不会收到 Push 通知。（可选）</param>
+        /// <param name="pushData">针对 iOS 平台为 Push 通知时附加到 payload 中，Android 客户端收到推送消息时对应字段名为 pushData.（可选）</param>
+        /// <param name="isPersisted">当前版本有新的自定义消息，而老版本没有该自定义消息时，老版本客户端收到消息后是否进行存储，0 表示为不存储、 1 表示为存储，默认为 1 存储消息.（可选）</param>
+        /// <param name="isCounted">当前版本有新的自定义消息，而老版本没有该自定义消息时，老版本客户端收到消息后是否进行未读消息计数，0 表示为不计数、 1 表示为计数，默认为 1 计数，未读消息数增加 1。（可选）</param>
+        public async Task<CodeSuccessReslut> PublishDiscussion(string fromUserId, string toDiscussionId, TxtMessage message, String pushContent, String pushData, int isPersisted, int isCounted)
+        {
+            if (string.IsNullOrEmpty(fromUserId))
+                throw new ArgumentNullException(nameof(fromUserId));
+            if (string.IsNullOrEmpty(toDiscussionId))
+                throw new ArgumentNullException("Paramer 'toDiscussionId' is required");
+
+            string postStr = "";
+            postStr += "fromUserId=" + WebUtility.UrlEncode(fromUserId == null ? "" : fromUserId) + "&";
+            postStr += "toDiscussionId=" + WebUtility.UrlEncode(toDiscussionId == null ? "" : toDiscussionId) + "&";
+            postStr += "objectName=" + WebUtility.UrlEncode(message.TYPE) + "&";
+            postStr += "content=" + WebUtility.UrlEncode(message.ToString()) + "&";
+            postStr += "pushContent=" + WebUtility.UrlEncode(pushContent == null ? "" : pushContent) + "&";
+            postStr += "pushData=" + WebUtility.UrlEncode(pushData == null ? "" : pushData) + "&";
+            postStr += "isPersisted=" + WebUtility.UrlEncode(Convert.ToString(isPersisted) == null ? "" : Convert.ToString(isPersisted)) + "&";
+            postStr += "isCounted=" + WebUtility.UrlEncode(Convert.ToString(isCounted) == null ? "" : Convert.ToString(isCounted)) + "&";
+            postStr = postStr.Substring(0, postStr.LastIndexOf('&'));
+
+            return JsonConvert.DeserializeObject<CodeSuccessReslut>(await RongHttpClient.ExecutePost(appKey, appSecret, RongCloud.RONGCLOUDURI + "/message/discussion/publish.json", postStr, "application/x-www-form-urlencoded"));
+        }
+
+        /// <summary>
+        /// 发送聊天室消息方法（一个用户向聊天室发送消息，单条消息最大 128k。每秒钟限 100 次。） 
+        /// </summary>
+        /// <param name="fromUserId">发送人用户Id（必传）</param>
+        /// <param name="toChatroomId">接收聊天室Id，提供多个本参数可以实现向多个聊天室发送消息（必传）</param>
+        /// <param name="message">发送消息内容（必传）</param>
+        public async Task<CodeSuccessReslut> PublishChatroom(string fromUserId, string[] toChatroomId, TxtMessage message)
+        {
+            if (string.IsNullOrEmpty(fromUserId))
+                throw new ArgumentNullException(nameof(fromUserId));
+            if (toChatroomId == null)
+                throw new ArgumentNullException(nameof(toChatroomId));
+
+            string postStr = "";
+            postStr += "fromUserId=" + WebUtility.UrlEncode(fromUserId == null ? "" : fromUserId) + "&";
+            for (int i = 0; i < toChatroomId.Length; i++)
+            {
+                string child = toChatroomId[i];
+                postStr += "toChatroomId=" + WebUtility.UrlEncode(child) + "&";
+            }
+
+            postStr += "objectName=" + WebUtility.UrlEncode(message.TYPE) + "&";
+            postStr += "content=" + WebUtility.UrlEncode(message.ToString()) + "&";
+            postStr = postStr.Substring(0, postStr.LastIndexOf('&'));
+
+            return JsonConvert.DeserializeObject<CodeSuccessReslut>(await RongHttpClient.ExecutePost(appKey, appSecret, RongCloud.RONGCLOUDURI + "/message/chatroom/publish.json", postStr, "application/x-www-form-urlencoded"));
+        }
+
+        /// <summary>
+        /// 发送广播消息方法（发送消息给一个应用下的所有注册用户，如用户未在线会对满足条件（绑定手机终端）的用户发送 Push 信息，单条消息最大 128k，会话类型为 SYSTEM。每小时只能发送 1 次，每天最多发送 3 次。） 
+        /// </summary>
+        /// <param name="fromUserId">发送人用户 Id（必传）</param>
+        /// <param name="message">文本消息</param>
+        /// <param name="pushContent">定义显示的 Push 内容，如果 objectName 为融云内置消息类型时，则发送后用户一定会收到 Push 信息. 如果为自定义消息，则 pushContent 为自定义消息显示的 Push 内容，如果不传则用户不会收到 Push 通知.（可选）</param>
+        /// <param name="pushData">针对 iOS 平台为 Push 通知时附加到 payload 中，Android 客户端收到推送消息时对应字段名为 pushData。（可选）</param>
+        /// <param name="os">针对操作系统发送 Push，值为 iOS 表示对 iOS 手机用户发送 Push ,为 Android 时表示对 Android 手机用户发送 Push ，如对所有用户发送 Push 信息，则不需要传 os 参数。（可选）</param>
+        public async Task<CodeSuccessReslut> Broadcast(string fromUserId, TxtMessage message, string pushContent, String pushData, String os)
+        {
+            if (fromUserId == null)
+                throw new ArgumentNullException(nameof(fromUserId));
+
+            string postStr = "";
+            postStr += "fromUserId=" + WebUtility.UrlEncode(fromUserId == null ? "" : fromUserId) + "&";
+            postStr += "objectName=" + WebUtility.UrlEncode(message.TYPE) + "&";
+            postStr += "content=" + WebUtility.UrlEncode(message.ToString()) + "&";
+            postStr += "pushContent=" + WebUtility.UrlEncode(pushContent == null ? "" : pushContent) + "&";
+            postStr += "pushData=" + WebUtility.UrlEncode(pushData == null ? "" : pushData) + "&";
+            postStr += "os=" + WebUtility.UrlEncode(os == null ? "" : os) + "&";
+            postStr = postStr.Substring(0, postStr.LastIndexOf('&'));
+
+            return JsonConvert.DeserializeObject<CodeSuccessReslut>(await RongHttpClient.ExecutePost(appKey, appSecret, RongCloud.RONGCLOUDURI + "/message/broadcast.json", postStr, "application/x-www-form-urlencoded"));
+        }
+
+        /// <summary>
+        /// 消息历史记录下载地址获取 方法消息历史记录下载地址获取方法。获取 APP 内指定某天某小时内的所有会话消息记录的下载地址。（目前支持二人会话、讨论组、群组、聊天室、客服、系统通知消息历史记录下载） 
+        /// </summary>
+        /// <param name="date">指定北京时间某天某小时，格式为2014010101,表示：2014年1月1日凌晨1点。（必传）</param>
+        public async Task<HistoryMessageReslut> GetHistory(string date)
+        {
+            if (string.IsNullOrEmpty(date))
+                throw new ArgumentNullException(nameof(date));
+
+            String postStr = "";
+            postStr += "date=" + WebUtility.UrlEncode(date == null ? "" : date) + "&";
+            postStr = postStr.Substring(0, postStr.LastIndexOf('&'));
+
+            return JsonConvert.DeserializeObject<HistoryMessageReslut>(await RongHttpClient.ExecutePost(appKey, appSecret, RongCloud.RONGCLOUDURI + "/message/history.json", postStr, "application/x-www-form-urlencoded"));
+        }
+
+        /// <summary>
+        /// 消息历史记录删除方法（删除 APP 内指定某天某小时内的所有会话消息记录。调用该接口返回成功后，date参数指定的某小时的消息记录文件将在随后的5-10分钟内被永久删除。） 
+        /// </summary>
+        /// <param name="date">指定北京时间某天某小时，格式为2014010101,表示：2014年1月1日凌晨1点。（必传）</param>
+        public async Task<CodeSuccessReslut> DeleteMessage(string date)
+        {
+            if (string.IsNullOrEmpty(date))
+                throw new ArgumentNullException(nameof(date));
+
+            string postStr = "";
+            postStr += "date=" + WebUtility.UrlEncode(date == null ? "" : date) + "&";
+            postStr = postStr.Substring(0, postStr.LastIndexOf('&'));
+
+            return JsonConvert.DeserializeObject<CodeSuccessReslut>(await RongHttpClient.ExecutePost(appKey, appSecret, RongCloud.RONGCLOUDURI + "/message/history/delete.json", postStr, "application/x-www-form-urlencoded"));
+        }
+    }
+}
---
+++ src/RongCloudNetCore/Methods/Push.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using RongCloudNetCore.Models;
+using RongCloudNetCore.Util;
+using System;
+using System.Threading.Tasks;
+
+namespace RongCloudNetCore.Methods
+{
+    public class Push
+    {
+        private string appKey;
+        private string appSecret;
+
+        public Push(string appKey, string appSecret)
+        {
+            this.appKey = appKey;
+            this.appSecret = appSecret;
+        }
+
+        /// <summary>
+        /// 添加 Push 标签方法 
+        /// </summary>
+        /// <param name="userTag">用户标签</param>
+        public async Task<CodeSuccessReslut> setUserPushTag(UserTag userTag)
+        {
+            if (userTag == null)
+                throw new ArgumentNullException(nameof(userTag));
+
+            string postStr = "";
+            postStr = JsonConvert.SerializeObject(userTag);
+            return JsonConvert.DeserializeObject<CodeSuccessReslut>(await RongHttpClient.ExecutePost(appKey, appSecret, RongCloud.RONGCLOUDURI + "/user/tag/set.json", postStr, "application/json"));
+        }
+
+        /// <summary>
+        /// 广播消息方法（fromuserid 和 message为null即为不落地的push） 
+        /// </summary>
+        /// <param name="pushMessage">json数据</param>
+        public async Task<CodeSuccessReslut> BroadcastPush(PushMessage pushMessage)
+        {
+            if (pushMessage == null)
+                throw new ArgumentNullException(nameof(pushMessage));
+
+            string postStr = "";
+            postStr = JsonConvert.SerializeObject(pushMessage);
+            return JsonConvert.DeserializeObject<CodeSuccessReslut>(await RongHttpClient.ExecutePost(appKey, appSecret, RongCloud.RONGCLOUDURI + "/push.json", postStr, "application/json"));
+        }
+    }
+}
