1b501ebd2e856ac30fe67c7aa649e7891bfd29b6
姚正发 <976691141@qq.com>
2017-02-15 13:23:39 +0800

    完成初版开发

--- src/RongCloudNetCore/RongCloud.cs
+++ src/RongCloudNetCore/RongCloud.cs
@@ -12,23 +12,23 @@ namespace RongCloudNetCore
         //确保线程同步
         private static readonly object locker = new object();
 
-        public User user;
-        public Message message;
-        public Wordfilter wordfilter;
-        public Group group;
-        public Chatroom chatroom;
-        public Push push;
-        public SMS sms;
+        public User User { get; set; }
+        public Message Message { get; set; }
+        public Wordfilter WordFilter { get; set; }
+        public Group Group { get; set; }
+        public Chatroom Chatroom { get; set; }
+        public Push Push { get; set; }
+        public SMS SMS { get; set; }
 
         private RongCloud(string appKey, string appSecret)
         {
-            user = new User(appKey, appSecret);
-            message = new Message(appKey, appSecret);
-            wordfilter = new Wordfilter(appKey, appSecret);
-            group = new Group(appKey, appSecret);
-            chatroom = new Chatroom(appKey, appSecret);
-            push = new Push(appKey, appSecret);
-            sms = new SMS(appKey, appSecret);
+            User = new User(appKey, appSecret);
+            Message = new Message(appKey, appSecret);
+            WordFilter = new Wordfilter(appKey, appSecret);
+            Group = new Group(appKey, appSecret);
+            Chatroom = new Chatroom(appKey, appSecret);
+            Push = new Push(appKey, appSecret);
+            SMS = new SMS(appKey, appSecret);
         }
 
         public static RongCloud GetInstance(string appKey, string appSecret)
---
+++ test/RongCloudNetCore.UnitTest/Test.cs
@@ -0,0 +1,75 @@
+﻿using RongCloudNetCore.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace RongCloudNetCore.UnitTest
+{
+    public class Test
+    {
+        public const string appKey = "82hegw5uhqtcx";
+        public const string appSecret = "FnngEX4S3aB";
+
+        private RongCloud _client;
+
+        public Test()
+        {
+            _client = RongCloud.GetInstance(appKey, appSecret);
+        }
+
+        [Fact]
+        public async Task TestGetUserToken()
+        {
+            var userToken = await _client.User.GetToken("userId1", "测试", "http://www.rongcloud.cn/images/logo.png");
+
+            Assert.Equal(userToken.Code, 200);
+            Assert.NotNull(userToken.Token);
+        }
+
+        [Fact]
+        public async Task TestRefreshUser()
+        {
+            var userToken = await _client.User.Refresh("userId1,", "测试", "http://www.rongcloud.cn/images/logo.png");
+            Assert.Equal(userToken.Code, 200);
+        }
+
+        [Fact]
+        public async Task TestCheckOnline()
+        {
+            var userCheck = await _client.User.CheckOnline("userId1");
+            Assert.Equal(userCheck.Code, 200);
+        }
+
+        [Fact]
+        public async Task TestBlock()
+        {
+            var userBlock = await _client.User.Block("userId1", 10);
+            Assert.Equal(userBlock.Code, 200);
+        }
+
+        [Fact]
+        public async Task TestUserUnBlock()
+        {
+            var userUnBlock = await _client.User.UnBlock("userId2");
+            Assert.Equal(userUnBlock.Code, 200);
+        }
+
+        [Fact]
+        public async Task TestQueryBlock()
+        {
+            var queryBlock = await _client.User.QueryBlock();
+            Assert.Equal(queryBlock.Code, 200);
+        }
+
+        [Fact]
+        public async Task TestPublishVoiceMessage()
+        {
+            string[] messageUserIds = { "userId2", "userid3", "userId4" };
+            VoiceMessage msg = new VoiceMessage("hello", "helloExtra", 20L);
+            var result = await _client.Message.PublishPrivate("userId1", messageUserIds, msg, "thisisapush", "exit", "4", 0, 0, 0);
+            Assert.Equal(result.Code, 200);
+        }
+    }
+}
